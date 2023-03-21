8bbf73f09e9488475d44ab5c0fdacea46a32f851
Neil Sullivan <ac30delay@gmail.com>
2023-03-21 09:43:19 -0500

    Initial greeting complete

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -9,6 +9,7 @@ namespace TabloidCLI.UserInterfaceManagers
 
         public IUserInterfaceManager Execute()
         {
+            Console.WriteLine("");
             Console.WriteLine("Main Menu");
 
             Console.WriteLine(" 1) Journal Management");
