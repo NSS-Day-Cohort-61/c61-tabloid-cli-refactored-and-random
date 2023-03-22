f6d5fd96a89b60344606f280f8cbc16aae860c00
kaci <kaci.school.info@gmail.com>
2023-03-22 10:07:22 -0500

    finished color management, edited blog detail manager and blog repo modules

--- TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -71,12 +71,13 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void ViewBlogPosts()
         {
-            List<Post> posts = _postRepository.GetByBlog(_blogId);
-            foreach (Post post in posts)
-            {
-                Console.WriteLine(post);
-            }
-            Console.WriteLine();
+            throw new NotImplementedException();
+            //List<Post> posts = _postRepository.GetByBlog(_blogId);
+            //foreach (Post post in posts)
+            //{
+            //    Console.WriteLine(post);
+            //}
+            //Console.WriteLine();
         }
 
         private void AddTag()
